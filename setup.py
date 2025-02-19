a44197214bcce6c1e8d46bab2a4063d52a6feb7d
ABHISHEK-343 <abhishek@1445zgmail.com>
2025-02-20 00:57:00 +0530

    this is my first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='abhishek',
+    author_email='abhishek1445z@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
